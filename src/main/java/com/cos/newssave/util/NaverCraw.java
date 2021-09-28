b113e45878ad7f8937886331885bb78e295c4cca
이 혜원(Hye-Won Lee) <HW.Lee@tsb.co.kr>
2021-09-28 16:57:47 +0900

    뉴스데이터

---
+++ src/main/java/com/cos/newssave/util/NaverCraw.java
@@ -0,0 +1,49 @@
+package com.cos.newssave.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import com.cos.newssave.domain.News;
+
+@Component
+public class NaverCraw {
+
+	int aidNum = 1;
+	
+	public List<News> collect2(){
+		RestTemplate rt = new RestTemplate();
+		List<News> newsList = new ArrayList<>();
+		
+		for (int i = 1; i < 6; i++) {
+			String aid = String.format("%010d", aidNum);
+			String url = "https://news.naver.com/main/read.naver?mode=LSD&mid=shm&sid1=102&oid=022&aid="+aid;
+			String html = rt.getForObject(url, String.class);
+
+			Document doc = Jsoup.parse(html);
+
+			Element titleElement = doc.selectFirst("#articleTitle");
+			Element createdAtElement = doc.selectFirst(".t11");
+			String title = titleElement.text();
+			String createdAt= createdAtElement.text();
+			
+			//System.out.println(title);
+			//System.out.println(time);
+			
+			News news = News.builder()
+					.title(title)
+					.createdAt(createdAt)
+					.build();
+			
+			newsList.add(news);
+			
+			aidNum ++;
+		}
+		return newsList;
+	}
+}
